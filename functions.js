a8767467c486a56374d1322e239d7bd780338eda
Carolyn GERLACH <EllaGerlach@Carolyns-MacBook-Air.local>
2017-03-01 08:06:58 -0800

    initial code

---
+++ functions.js
@@ -0,0 +1,46 @@
+/**
+* Removes part of string between two sub strings
+* @param {string} text The original string
+* @param {string} start The starting string
+* @param {string} end The ending string
+* @return {string} The string in between
+* @throws (Error} If start or end not found
+*/
+function between(string, start, end) {
+    var startAt = string.indexOf(start);
+
+    if (startAt == -1) {
+        throw new Error("No start found: " + start);
+    }
+
+    startAt += start.length;
+    var endAt = string.indexOf(end, startAt);
+
+    if (endAt == -1) {
+        throw new Error("No end found: " + end);
+    }
+
+    return string.slice(startAt, endAt);
+}
+
+/**
+ * Returns an area code from a phone number: (###) ###-####
+ * @param   {string} phoneNum The phone number
+ * @returns {string} The area code * @throws {Error} If the format is incorrect
+ */
+function getAreaCode(phoneNum) {
+
+    var areaCode;
+
+    try {
+        areaCode = between(phoneNum, "(", ")");
+        areaCode = areaCode.trim();
+        if (areaCode.length == 3 && Number(areaCode)) {
+            return areaCode;
+        } else {
+            throw new Error("Invalid area code: " + areaCode);
+        }
+    } catch (error) {
+        throw new Error("Invalid phone number: " + error.message);
+    }
+}
