67c43303b27b8025885ad577da8dcd52f4ff1ab7
Carolyn GERLACH <EllaGerlach@Carolyns-MacBook-Air.local>
2017-03-03 07:58:08 -0800

    function for get line code and on click button for line code

--- functions.js
+++ functions.js
@@ -65,3 +65,46 @@ function displayAreaCode(inputId, outputId) {
 
     document.getElementById(outputId).innerHTML = outputText;
 }
+
+/**
+ * Returns an line code from a phone number: (###) ###-####
+ * @param   {string} phoneNum The phone number
+ * @returns {string} The line code * @throws {Error} If the format is incorrect
+ */
+function getLineCode(phoneNum) {
+
+     var lineCode;
+
+    try {
+        lineCode = between(phoneNum, "-", "");
+        lineCode = lineCode.trim();
+        if (lineCode.length == 4 && Number(lineCode)) {
+            return lineCode;
+        } else {
+            throw new Error("Invalid line code: " + lineCode);
+        }
+    } catch (error) {
+        throw new Error("Invalid phone number: " + error.message);
+    }
+}
+
+/**
+ * Displays the line code for an inputted phone number
+ * @param {string} inputId  The element id for the text box
+ * @param {string} outputId The element id of message div
+ */
+function displayLineCode(inputId, outputId) {
+    var outputText = "";
+    var phoneNum = document.getElementById(inputId).value;
+
+    // Now try to get the code
+    try {
+        var lineCode = getLineCode(phoneNum);
+        outputText = "Your line code is " + lineCode;
+    } catch (error) {
+        console.log(error.message);
+        outputText = error.message;
+    }
+
+    document.getElementById(outputId).innerHTML = outputText;
+}
