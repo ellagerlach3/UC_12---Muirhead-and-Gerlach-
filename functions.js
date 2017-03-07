b70ce6846f9d17674f8b46ea6ec3f78bd28b1aca
Brianna B Muirhead <bm66627@srcs.loc>
2017-03-07 09:33:58 -0800

    added co code to html and createdf siplay functiob

--- functions.js
+++ functions.js
@@ -60,6 +60,22 @@ function getCoCode(phoneNum) {
     }
 }
 
+function displayCoCode(inputId, outputId) {
+    var outputText = "";
+    var phoneNum = document.getElementById(inputId).value;
+
+    // Now try to get the code
+    try {
+        var coCode = getCoCode(phoneNum);
+        outputText = "Your CO code is " + coCode;
+    } catch (error) {
+        console.log(error.message);
+        outputText = error.message;
+    }
+
+    document.getElementById(outputId).innerHTML = outputText;
+}
+
 /**
  * Displays the area code for an inputted phone number
  * @param {string} inputId  The element id for the text box
